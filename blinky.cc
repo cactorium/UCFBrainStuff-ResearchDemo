4dec34668ba858a73b2876111004010d544a43da
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-09-17 10:35:20 -0400

    Separate out graphics logic from main function

--- blinky.cc
+++ blinky.cc
@@ -1,106 +1,22 @@
 // Lots of stuff copied from lots of OpenGL tutorials!
-
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-#include <cstdio>
-#include <cstdint>
-
 #include <algorithm>
-#include <fstream>
 #include <iostream>
 #include <vector>
 
+#include "window.h"
 
-static void error_callback(int error, const char* description) {
+void error_callback(int error, const char* description);
+
+void error_callback(int error, const char* description) {
     fputs(description, stderr);
-}
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
-static void window_size_callback(GLFWwindow* window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path) {
-    // Create the shaders
-    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
- 
-    // Read the Vertex Shader code from the file
-    std::string VertexShaderCode;
-    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-    if(VertexShaderStream.is_open())
-    {
-        std::string Line = "";
-        while(getline(VertexShaderStream, Line))
-            VertexShaderCode += "\n" + Line;
-        VertexShaderStream.close();
-    }
- 
-    // Read the Fragment Shader code from the file
-    std::string FragmentShaderCode;
-    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-    if(FragmentShaderStream.is_open()){
-        std::string Line = "";
-        while(getline(FragmentShaderStream, Line))
-            FragmentShaderCode += "\n" + Line;
-        FragmentShaderStream.close();
-    }
- 
-    GLint Result = GL_FALSE;
-    int InfoLogLength;
- 
-    // Compile Vertex Shader
-    printf("Compiling shader : %s\n", vertex_file_path);
-    char const * VertexSourcePointer = VertexShaderCode.c_str();
-    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
-    glCompileShader(VertexShaderID);
- 
-    // Check Vertex Shader
-    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> VertexShaderErrorMessage(InfoLogLength);
-    glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-    fprintf(stdout, "%s\n", &VertexShaderErrorMessage[0]);
- 
-    // Compile Fragment Shader
-    printf("Compiling shader : %s\n", fragment_file_path);
-    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
-    glCompileShader(FragmentShaderID);
- 
-    // Check Fragment Shader
-    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> FragmentShaderErrorMessage(InfoLogLength);
-    glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-    fprintf(stdout, "%s\n", &FragmentShaderErrorMessage[0]);
- 
-    // Link the program
-    fprintf(stdout, "Linking program\n");
-    GLuint ProgramID = glCreateProgram();
-    glAttachShader(ProgramID, VertexShaderID);
-    glAttachShader(ProgramID, FragmentShaderID);
-    glLinkProgram(ProgramID);
- 
-    // Check the program
-    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> ProgramErrorMessage( std::max(InfoLogLength, int(1)) );
-    glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-    fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
- 
-    glDeleteShader(VertexShaderID);
-    glDeleteShader(FragmentShaderID);
- 
-    return ProgramID;
 }
 
 uint32_t next_msequence63(uint32_t i) {
     const auto lsb = i & 0x00000001;
-    const auto lsb2 = i & (0x00000001 << 5);
+    const auto lsb2 = (i & (0x00000001 << 5)) >> 5;
     return (i >> 1) | ((lsb ^ lsb2) << 5);
 };
 
@@ -122,46 +38,7 @@ int main(void) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
-    // Create and compile our GLSL program from the shaders
-    GLuint programID = LoadShaders("vert.vsh", "frag.fsh");
-
-    glfwSwapInterval(1);
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetWindowSizeCallback(window, window_size_callback);
-
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-    // An array of 3 vectors which represents 3 vertices
-    static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, 1.0f, 0.0f,
-        1.0f,  1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        -1.0f, -1.0f, 0.0f
-    };
-    static const GLuint g_index_buffer_data[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
-    // This will identify our vertex buffer
-    GLuint vertexbuffer, indexbuffer;
-     
-    // Generate 1 buffer, put the resulting identifier in vertexbuffer
-    glGenBuffers(1, &vertexbuffer);
-     
-    // The following commands will talk about our 'vertexbuffer' buffer
-    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-     
-    // Give our vertices to OpenGL.
-    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-    glGenBuffers(1, &indexbuffer);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_index_buffer_data)*sizeof(GLuint), g_index_buffer_data, GL_STATIC_DRAW);
-
-    GLint uniformLocation = glGetUniformLocation(programID, "vals");
-    std::cerr << "uniform location is " << uniformLocation << std::endl;
+    SetupWindow(window);
 
     auto seqs = std::vector<uint32_t>(16);
     uint32_t tmp = 1;
@@ -172,39 +49,17 @@ int main(void) {
         }
     }
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     while (!glfwWindowShouldClose(window)) {
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-           0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-           3,                  // size
-           GL_FLOAT,           // type
-           GL_FALSE,           // normalized?
-           0,                  // stride
-           (void*)0            // array buffer offset
-        );
-
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         uint32_t vals = 0;
         for (auto i = 0u; i < seqs.size(); i++) {
             vals |= (seqs[i] & 1) << i;
         }
-         
-        // Draw the triangle !
-        glUseProgram(programID);
-        glUniform1ui(uniformLocation, vals);
-
-        // Bind the index array
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
-        // glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-        glDrawElements(GL_TRIANGLES, sizeof(g_index_buffer_data), GL_UNSIGNED_INT, (void*)0);
-         
-        glDisableVertexAttribArray(0);
-        glfwPollEvents();
+        DrawFrame(vals);
         std::for_each(seqs.begin(), seqs.end(), [](uint32_t &val) {
             val = next_msequence63(val);
         });
+        glfwPollEvents();
         glfwSwapBuffers(window);
     }
     glfwDestroyWindow(window);
---
+++ window.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <cstdio>
+#include <cstdint>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+struct GLState;
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void window_size_callback(GLFWwindow* window, int width, int height);
+
+GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path);
+
+void SetupWindow(GLFWwindow *window);
+void DrawFrame(uint32_t uniformVal);
+
+#endif
