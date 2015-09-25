a0b850964f8230049b39ed1a9314c95a2cf84368
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-09-25 05:57:18 -0400

    Save to push

--- processor.cc
+++ processor.cc
@@ -1,6 +1,88 @@
+#include <cmath>
 #include <iostream>
 
 #include "processor.h"
+
+static int getValueBySensorId(const SensorPosition &p, const Emotiv::Frame &f) {
+    switch (p) {
+        case F3: return f.F3;
+        case FC6: return f.FC6;
+        case P7: return f.P7;
+        case T8: return f.T8;
+        case F7: return f.F7;
+        case F8: return f.F8;
+        case T7: return f.T7;
+        case P8: return f.P8;
+        case AF4: return f.AF4;
+        case F4: return f.F4;
+        case AF3: return f.AF3;
+        case O2: return f.O2;
+        case O1: return f.O1;
+        case FC5: return f.FC5;
+        default:
+            return 0;
+    }
+}
+
+static short getQualityBySensorId(const SensorPosition &p, const Emotiv::Frame &f) {
+    switch (p) {
+        case F3: return f.cq.F3;
+        case FC6: return f.cq.FC6;
+        case P7: return f.cq.P7;
+        case T8: return f.cq.T8;
+        case F7: return f.cq.F7;
+        case F8: return f.cq.F8;
+        case T7: return f.cq.T7;
+        case P8: return f.cq.P8;
+        case AF4: return f.cq.AF4;
+        case F4: return f.cq.F4;
+        case AF3: return f.cq.AF3;
+        case O2: return f.cq.O2;
+        case O1: return f.cq.O1;
+        case FC5: return f.cq.FC5;
+        default:
+            return 0;
+    }
+}
+
+EmotivProcessor::EmotivProcessor(): mode(TRAINING), frame_num(0), training_num(0) {
+    for (auto s : kSensorOrder) {
+        auto idx = static_cast<int>(s);
+        processors[idx] = SignalProcessor(s);
+    }
+}
+
+void EmotivProcessor::ProcessFrame(const Emotiv::Frame &f, bool isSyncFrame) {
+    std::cerr << "Got frame data for frame " << frame_num << std::endl;
+
+    if (isSyncFrame) {
+        frame_num = 0;
+    }
+    for (auto &p : processors) {
+        p.ProcessFrame(frame_num, f, isSyncFrame);
+    }
+    frame_num++;
+
+    if (std::count_if(processors.begin(), processors.end(),
+                [](const SignalProcessor &p) -> bool {
+                    return p.TrainingFinished();
+                }) >= kSensors/3) {
+        this -> SetMode(PROCESSING);
+    }
+}
+
+SignalResult EmotivProcessor::GetProcessingResult() const {
+    SignalResult best;
+    best.confidence = 0.0f;
+
+    for (auto &p: processors) {
+        auto result = p.GetProcessingResult();
+        if (result.confidence > best.confidence) {
+            best = result;
+        }
+    }
+    return best;
+}
 
 void EmotivProcessor::SetMode(DemoMode m) {
     if (m == TRAINING) {
@@ -8,12 +90,111 @@ void EmotivProcessor::SetMode(DemoMode m) {
     } else {
         std::cerr << "training mode set to processing" << std::endl;
     }
+
+    for (auto s : kSensorOrder) {
+        processors[static_cast<int>(s)].SetMode(m);
+    }
+}
+
+SignalProcessor::SignalProcessor(SensorPosition pos): mode(TRAINING),
+    pos(pos), first_sync(true), avg_quality(0.0f),
+    last_corr(-1), confidence(0.0f) {;}
+
+void SignalProcessor::ProcessFrame(int frame_num, const Emotiv::Frame &f,
+        bool isSyncFrame) {
+    if (mode == TRAINING) {
+        if (isSyncFrame) {
+            if (first_sync) {
+                first_sync = false;
+            } else {
+                if (avg_quality > kQualityThreshold && tmp.size() > 0) {
+                    auto new_data = Eigen::VectorXf(tmp.size());
+                    for (auto i = 0; i < tmp.size(); i++) {
+                        new_data(i) = tmp[i];
+                    }
+                    training_data.push_back(new_data);
+                    data_quality.push_back(avg_quality);
+                }
+                tmp.clear();
+                avg_quality = 0.0f;
+            }
+        }
+        tmp.push_back(getValueBySensorId(pos, f));
+        avg_quality = avg_quality*frame_num/(1+frame_num) +
+            getQualityBySensorId(pos, f)/(1+frame_num);
+    } else {
+        for (auto i = 0; i < last.size() - 1; i++) {
+            last(i) = last(i + 1);
+        }
+        last(last.size()-1) = getValueBySensorId(pos, f);
+        for (auto i = 0; i < last_quality.size() - 1; i++) {
+            last_quality(i) = last_quality(i + 1);
+        }
+        last_quality(last_quality.size()-1) = getQualityBySensorId(pos, f);
+
+        auto shifted_templ = templ;
+        auto best = -1e-100f;
+        auto offset = -1;
+        for (auto i = 0; i < shifted_templ.size(); i++) {
+            auto tmp = shifted_templ(0);
+            for (auto j = 0; j < shifted_templ.size() - 1; j++) {
+                shifted_templ(j) = shifted_templ(j + 1);
+            }
+            // TODO
+        }
+    }
+}
+
+SignalResult SignalProcessor::GetProcessingResult() const {
+    return SignalResult {
+        last_corr, confidence
+    };
+}
+
+bool SignalProcessor::TrainingFinished() const {
+    return training_data.size() >= kTrainingSets;
+}
+
+void SignalProcessor::SetMode(DemoMode m) {
     mode = m;
+    if (m == TRAINING) {
+        first_sync = true;
+        avg_quality = 0.0f;
+        training_data.clear();
+        data_quality.clear();
+        tmp.clear();
+    } else {
+        if (training_data.size() <= 0) {
+            std::cerr << "WARNING: no new training data, keeping old values" << std::endl;
+            return;
+        }
+
+        int trunc_size = 100000000;
+        for (auto &s: training_data) {
+            if (s.size() < trunc_size) {
+                trunc_size = s.size();
+            }
+        }
+
+        // average and generate template
+        auto templa = Eigen::VectorXf(trunc_size);
+        for (auto i = 0; i < templa.size(); i++) {
+            auto sum = 0.0f;
+            for (auto s: training_data) {
+                sum += static_cast<float>(s(i));
+            }
+            templa(i) = sum/training_data.size();
+        }
+
+        auto qual_sum = std::accumulate(data_quality.begin(), data_quality.end(), 0.0f);
+
+        templ = templa/sqrtf(templa.dot(templa));
+        last = Eigen::VectorXf(trunc_size);
+        last_quality = Eigen::VectorXf(trunc_size);
+
+        last.fill(0.0f);
+        last_quality.fill(0.0f);
+        last_corr = -1;
+        confidence = qual_sum/data_quality.size();
+    }
 }
-
-void EmotivProcessor::ProcessFrame(const Emotiv::Frame &f, bool isSyncFrame) {
-    std::cerr << "Got frame data for frame " << frame_num << std::endl;
-
-    frame_num++;
-}
-
--- processor.h
+++ processor.h
@@ -7,26 +7,74 @@
 
 #include <Eigen/Dense>
 
-#include <array>
 #include <vector>
+
+const int kTrainingSets = 10;
+const int kSensors = 14;
+const float kQualityThreshold = 5.0f;
 
 enum DemoMode {
     TRAINING,
     PROCESSING
 };
 
+enum SensorPosition {
+	F3, FC6, P7, T8, F7, F8, T7, P8, AF4, F4, AF3, O2, O1, FC5, INVALID
+};
+
+const std::array<SensorPosition, kSensors> kSensorOrder {{
+    F3, FC6, P7, T8, F7, F8, T7, P8, AF4, F4, AF3, O2, O1, FC5
+}};
+
+struct SignalResult {
+    int offset;
+    float confidence;
+};
+
+class SignalProcessor {
+public:
+    SignalProcessor() : SignalProcessor(INVALID) {;}
+    SignalProcessor(SensorPosition pos);
+    ~SignalProcessor() {;}
+
+    void ProcessFrame(int frame_num, const Emotiv::Frame &f, bool isSyncFrame);
+    SignalResult GetProcessingResult() const;
+    bool TrainingFinished() const;
+    void SetMode(DemoMode m);
+protected:
+    // used for both modes
+    DemoMode mode;
+    SensorPosition pos;
+
+    // used for training mode
+    bool first_sync;
+    float avg_quality;
+    std::vector<Eigen::VectorXf> training_data;
+    std::vector<float> data_quality;
+    std::vector<float> tmp;
+
+    // used for processing mode
+    Eigen::VectorXf templ;
+    Eigen::VectorXf last;
+    Eigen::VectorXf last_quality;
+    int last_corr;
+    float confidence;
+};
+
 class EmotivProcessor {
 public:
-    EmotivProcessor(): mode(TRAINING), frame_num(0) {;}
+    EmotivProcessor();
     ~EmotivProcessor() {;}
 
     void ProcessFrame(const Emotiv::Frame &f, bool isSyncFrame);
+    SignalResult GetProcessingResult() const;
 
     void SetMode(DemoMode m);
-    DemoMode Mode() {return mode;}
+    DemoMode Mode() const {return mode;}
 protected:
     DemoMode mode;
-    int frame_num;
+    int frame_num, training_num;
+    std::array<SignalProcessor, kSensors> processors;
 };
 
 #endif
