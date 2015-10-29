aa58df68ef9dd485ffcb6e4448f9f0a1064daae5
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-10-28 20:47:32 -0400

    Arbitrary restructuring

--- py/flashy.py
+++ py/flashy.py
@@ -13,33 +13,6 @@ import numpy as np
 FLOAT_SIZE = 4
 UINT_SIZE = 4
 
-
-def window_size_callback(window, width, height):
-  print("resize: {} {}".format(width, height))
-  gl.glViewport(0, 0, width, height)
-
-
-if not glfw.Init():
-  exit()
-
-
-glfw.WindowHint(glfw.CONTEXT_VERSION_MAJOR, 3)
-glfw.WindowHint(glfw.CONTEXT_VERSION_MINOR, 2)
-glfw.WindowHint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
-glfw.WindowHint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-
-window = glfw.CreateWindow(640, 480, 'Hello World')
-if not window:
-  glfw.Terminate()
-  exit()
-
-glfw.SetWindowSizeCallback(window, window_size_callback)
-glfw.MakeContextCurrent(window)
-
-program = gl.glCreateProgram()
-vertex = gl.glCreateShader(gl.GL_VERTEX_SHADER)
-fragment = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
-
 vertex_code = '''
 #version 300 es
 
@@ -86,24 +59,6 @@ void main(){
 }
 '''
 
-gl.glShaderSource(vertex, vertex_code)
-gl.glShaderSource(fragment, fragment_code)
-
-gl.glCompileShader(vertex)
-gl.glCompileShader(fragment)
-if gl.glGetShaderiv(vertex, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
-  raise RuntimeError(gl.glGetShaderInfoLog(vertex))
-if gl.glGetShaderiv(fragment, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
-  raise RuntimeError(gl.glGetShaderInfoLog(fragment))
-
-gl.glAttachShader(program, vertex)
-gl.glAttachShader(program, fragment)
-
-gl.glLinkProgram(program)
-
-gl.glDetachShader(program, vertex)
-gl.glDetachShader(program, fragment)
-
 vertex_data = np.array([
     -1.0, 1.0, 0.0,
     1.0,  1.0, 0.0,
@@ -116,31 +71,10 @@ index_buffer_data = np.array([
     2, 3, 0],
     dtype=np.uint32)
 
-vao = gl.glGenVertexArrays(1)
-gl.glBindVertexArray(vao)
-print("vertex array object: {}".format(vao))
 
-vbo = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
-gl.glBufferData(gl.GL_ARRAY_BUFFER,
-                vertex_data.size*FLOAT_SIZE,
-                vertex_data,
-                gl.GL_STATIC_DRAW)
-print("vertex vbo object: {}".format(vbo))
-
-ibo = gl.glGenBuffers(1)
-gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ibo)
-gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,
-                index_buffer_data.size*UINT_SIZE,
-                index_buffer_data,
-                gl.GL_STATIC_DRAW)
-print("vertex ibo object: {}".format(ibo))
-
-valsId = gl.glGetUniformLocation(program, "vals")
-chosenId = gl.glGetUniformLocation(program, "chosen")
-
-print("vals location is {}".format(valsId))
-print("chosen location is {}".format(chosenId))
+def window_size_callback(window, width, height):
+  print("resize: {} {}".format(width, height))
+  gl.glViewport(0, 0, width, height)
 
 
 def next_msequence63(i):
@@ -175,6 +109,70 @@ def pack_lights(ls):
   for idx, l in enumerate(ls):
     val = val | ((l & 1) << idx)
   return val
+
+if not glfw.Init():
+  exit()
+
+glfw.WindowHint(glfw.CONTEXT_VERSION_MAJOR, 3)
+glfw.WindowHint(glfw.CONTEXT_VERSION_MINOR, 2)
+glfw.WindowHint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+glfw.WindowHint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+window = glfw.CreateWindow(640, 480, 'Hello World')
+if not window:
+  glfw.Terminate()
+  exit()
+
+glfw.SetWindowSizeCallback(window, window_size_callback)
+glfw.MakeContextCurrent(window)
+
+program = gl.glCreateProgram()
+vertex = gl.glCreateShader(gl.GL_VERTEX_SHADER)
+fragment = gl.glCreateShader(gl.GL_FRAGMENT_SHADER)
+
+gl.glShaderSource(vertex, vertex_code)
+gl.glShaderSource(fragment, fragment_code)
+
+gl.glCompileShader(vertex)
+gl.glCompileShader(fragment)
+if gl.glGetShaderiv(vertex, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
+  raise RuntimeError(gl.glGetShaderInfoLog(vertex))
+if gl.glGetShaderiv(fragment, gl.GL_COMPILE_STATUS) != gl.GL_TRUE:
+  raise RuntimeError(gl.glGetShaderInfoLog(fragment))
+
+gl.glAttachShader(program, vertex)
+gl.glAttachShader(program, fragment)
+
+gl.glLinkProgram(program)
+
+gl.glDetachShader(program, vertex)
+gl.glDetachShader(program, fragment)
+
+vao = gl.glGenVertexArrays(1)
+gl.glBindVertexArray(vao)
+print("vertex array object: {}".format(vao))
+
+vbo = gl.glGenBuffers(1)
+gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
+gl.glBufferData(gl.GL_ARRAY_BUFFER,
+                vertex_data.size*FLOAT_SIZE,
+                vertex_data,
+                gl.GL_STATIC_DRAW)
+print("vertex vbo object: {}".format(vbo))
+
+ibo = gl.glGenBuffers(1)
+gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, ibo)
+gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER,
+                index_buffer_data.size*UINT_SIZE,
+                index_buffer_data,
+                gl.GL_STATIC_DRAW)
+print("vertex ibo object: {}".format(ibo))
+
+valsId = gl.glGetUniformLocation(program, "vals")
+chosenId = gl.glGetUniformLocation(program, "chosen")
+
+print("vals location is {}".format(valsId))
+print("chosen location is {}".format(chosenId))
 
 
 while not glfw.WindowShouldClose(window):
