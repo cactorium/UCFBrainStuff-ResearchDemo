c83a8f69033b92ee2181b29ab4b72c59602f8c88
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-11-18 10:24:39 -0500

    Feed all channels into CCA, cf. Bin Gao, et al.

--- py/braingerZone.py
+++ py/braingerZone.py
@@ -8,7 +8,7 @@ from sklearn.cross_decomposition import CCA
 
 import numpy as np
 import sys
-import math
+# import math
 
 sensor_names = ['F3', 'FC6', 'P7', 'T8', 'F7', 'F8', 'T7', 'P8', 'AF4',
                 'F4', 'AF3', 'O2', 'O1', 'FC5', 'X', 'Y']
@@ -30,7 +30,7 @@ def resize_and_shift(in_vec, sz):
 class State(object):
   TRAINING = 0
   PROCESSING = 1
-  N_STIM_CYCLES = 10
+  N_STIM_CYCLES = 9  # 200
   SEQUENCE_SIZE = 134
   NUM_FLASHERS = 16
   RECORDING = True
@@ -60,69 +60,52 @@ class State(object):
   def process_training_data(self):
     last_start = [f for f in self.t_sync_frames if
                   (f + State.SEQUENCE_SIZE) < self.seq_num][-1]
-    last_end = sorted([f for f in self.t_sync_frames if f > last_start])[0]
-    starts = [f for f in self.t_sync_frames if f < last_start]
+    # last_end = sorted([f for f in self.t_sync_frames if f > last_start])[0]
+    starts = [f for f in self.t_sync_frames if f <= last_start]
     ends = [sorted([f for f in self.t_sync_frames if f > start])[0]
             for start in starts]
     frame_pairs = zip(starts, ends)
     print "Frame pairs: ", frame_pairs
-    # find the most prominent channel
-    best_channel, best_corr = None, None    # luckily None < all numbers
-    best_avg = np.zeros(State.SEQUENCE_SIZE)
-    for s in sensor_names:
-      cur_set = self.t_data[s]
-      # average across all the other frames
-      avg = np.zeros(State.SEQUENCE_SIZE)
-      for start, end in frame_pairs:
-        tmp = resize_and_shift(cur_set[start:end], avg.size)
-        avg = avg + tmp
-
-      avg = avg * 1.0/len(frame_pairs)
-      channel = resize_and_shift(cur_set[last_start:last_end], avg.size)
-      channel_mag, avg_mag = np.dot(channel, channel), np.dot(avg, avg)
-      if channel_mag == 0.0 or avg_mag == 0.0:
-        print "Skipping %s because of lack of values" % s
-        continue
-      cur_corr = np.dot(avg, channel)/math.sqrt(channel_mag*avg_mag)
-      print "%s: corr %f" % (s, cur_corr)
-      if cur_corr > best_corr:
-        best_channel, best_corr, best_avg = s, cur_corr, avg
-
-    print "Best channel %s with corr %f" % (best_channel, best_corr)
     # calculate CCA weights for it
     self.cca = CCA(n_components=1)
     x_t = np.zeros((len(sensor_names), State.SEQUENCE_SIZE*len(frame_pairs)))
-    y_t = np.zeros((1, State.SEQUENCE_SIZE*len(frame_pairs)))
+    y_t = np.zeros((len(sensor_names), State.SEQUENCE_SIZE*len(frame_pairs)))
     # copy the data into an array for processing via CCA
     for idx, s in enumerate(sensor_names):
       for (f_idx, (st, ed)) in enumerate(frame_pairs):
         s_idx, e_idx = f_idx*State.SEQUENCE_SIZE, (f_idx+1)*State.SEQUENCE_SIZE
         x_t[idx][s_idx:e_idx] = resize_and_shift(
             self.t_data[s][st:ed], State.SEQUENCE_SIZE)
-        if s == best_channel:
-          y_t[0][s_idx:e_idx] = best_avg
+      x_avg = x_t[idx].reshape((State.SEQUENCE_SIZE, -1)).mean(1)
+      y_t[idx] = np.tile(x_avg, len(frame_pairs))
+      x_mag = np.dot(x_t[idx], x_t[idx])
+      y_mag = np.dot(y_t[idx], y_t[idx])
+      if x_mag != 0 and y_mag != 0:
+        s_corr = np.dot(y_t[idx], x_t[idx])/(np.sqrt(x_mag * y_mag))
+        print "%s correlation: %f" % (s, s_corr)
+      else:
+        print "%s no data" % s
 
     x = x_t.transpose()
     y = y_t.transpose()
+    # print x.shape, y.shape
     self.cca.fit(x, y)
     print "CCA weights: "
     print self.cca.x_weights_
 
     cca_channel = self.cca.transform(x).flatten()
-    # print cca_channel.shape
-    n_tile = len(cca_channel)/State.SEQUENCE_SIZE
-    self.cca_templ = cca_channel.reshape((State.SEQUENCE_SIZE, -1)).mean(axis=1)
-    cca_avg = np.tile(self.cca_templ, n_tile)
-    print cca_avg.shape
-    print cca_avg
-    cca_channel_mag = np.dot(cca_channel, cca_channel)
+    self.cca_templ = cca_channel.reshape(
+        (State.SEQUENCE_SIZE, len(frame_pairs))).mean(axis=1)
+    cca_avg = np.tile(self.cca_templ, len(frame_pairs))
     cca_avg_mag = np.dot(cca_avg, cca_avg)
-    print cca_channel_mag, cca_avg_mag
-    cca_self_corr = np.dot(cca_channel, cca_avg)/(math.sqrt(
-        cca_channel_mag*cca_avg_mag))
-    print "cca channel self corr: %f" % cca_self_corr
+
+    cca_channel_mag = np.dot(cca_channel, cca_channel)
+
+    cca_self_corr = np.dot(cca_channel, cca_avg)/(np.sqrt(
+        np.multiply(cca_channel_mag, cca_avg_mag)))
+    print "cca channel self corr: ", cca_self_corr
     #  train OCSVM on dataset
-    raise "I don't know what I'm doing!"
+    raise TypeError("I don't know what I'm doing!")
     return cca_channel
 
   def process_training(self, packet, is_sync_frame):
@@ -135,7 +118,7 @@ class State(object):
         self.t_data = {s: np.zeros(0) for s in sensor_names}
 
       self.t_sync_frames.append(self.seq_num)
-      if len(self.t_sync_frames) > State.N_STIM_CYCLES:
+      if len(self.t_sync_frames) > State.N_STIM_CYCLES + 1:
           self.state = State.PROCESSING
           self.process_training_data()
     for s in sensor_names:
--- py/eeg_test.py
+++ py/eeg_test.py
@@ -4,7 +4,7 @@ import time
 
 import braingerZone as bz
 
-SPEED_UP = 8
+SPEED_UP = 200
 
 
 class TestData(object):
