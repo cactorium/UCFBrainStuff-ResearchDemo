28ad629ddedfc97f23d19b566b97db1fa5fdb1b8
cameron-johnson <cameronjohnson2011@knights.ucf.edu>
2015-11-20 12:47:56 -0500

    it's working kind of ish

--- py/braingerZone.py
+++ py/braingerZone.py
@@ -11,35 +11,67 @@ import scipy.signal as spsig
 import sys
 
 import time
-import cPickle`
+import cPickle
 
+
+sensor_names = ['F3', 'FC6', 'P7', 'T8', 'F7', 'F8', 'T7', 'P8', 'AF4',
+                'F4', 'AF3', 'O2', 'O1', 'FC5', 'X', 'Y']
+
+def calculate_eeg_val(packet):
+  vld_vals = [packet.sensors[x]['value']
+                 for x in sensor_names if True or packet.sensors[x]['quality'] > 5]
+  ref_avg = 0
+  if len(vld_vals) > 0:
+      ref_avg = sum(vld_vals)/float(len(vld_vals))
+  # average the two visual cortex electrodes
+  vld_data = [packet.sensors[x]['value'] for x in ['O1', 'O2'] if
+                packet.sensors[x]['quality'] > 5]
+  avg_val = 0.0
+  if len(vld_data) > 0:
+    avg_val = sum(vld_data)/float(len(vld_data))
+#    ref_avg = 0
+
+  # subtract general EEG activity to accentuate visual cortex activity
+  return avg_val - ref_avg
+
+def plot_fft(buf):
+  f, fft = spsig.welch(buf, fs=128.0) # , nfft=512)
+  print 'plot!'
+  plt.clf()
+  plt.plot(f, fft)
+  # plt.xlim([4, 20])
+  plt.ylim([0, 120])
+  # plt.ylim([0.5e-3, 1])
+  plt.xlabel('frequency [Hz]')
+  plt.ylabel('PSD [V**2/Hz]')
+  # plt.specgram(fft_data, NFFT=256, Fs=128, noverlap=128)
+  plt.draw()
 
 class State(object):
   TRAINING = 0
   PROCESSING = 1
-  WINDOw = 384
+  WINDOW = 384
   NUM_CHANNELS = 16
 
   def __init__(self):
-    # self.state = State.TRAINING
+    self.state = State.TRAINING
     self.state = State.PROCESSING
     self.raw_buf = np.zeros((2*State.WINDOW))
+    self.training_buf = np.zeros(0)
     self.idx = 0
+    plt.ion()
+    plt.show()
 
   def process_frame(self, packet, is_sync_frame, chosen_val):
-    sensor_names = ['F3', 'FC6', 'P7', 'T8', 'F7', 'F8', 'T7', 'P8', 'AF4',
-                    'F4', 'AF3', 'O2', 'O1', 'FC5', 'X', 'Y']
-
     if self.state == State.TRAINING:
-      pass
+      self.training_buf = np.append(self.training_buf, [calculate_eeg_val(packet)])
+      #print self.idx
+      if self.idx % 512 == 256:
+          print 'base line plot!'
+          plot_fft(self.training_buf - self.training_buf.mean())
+      self.idx = self.idx + 1
     elif self.state == State.PROCESSING:
-      ref_avg = sum([packet.sensors[x]['value']
-                     for x in sensor_names])/len(sensor_names)
-      # average the two visual cortex electrodes
-      avg_val = sum([packet.sensors[x]['value'] for x in ['O1', 'O2']])/2.0
-
-      # subtract general EEG activity to accentuate visual cortex activity
-      new_val = avg_val - ref_avg
+      new_val = calculate_eeg_val(packet)
 
       # fancy buffering trick to make the ring buffer easier to manage
       # basically by putting it in two places, it makes it possible
@@ -50,17 +82,12 @@ class State(object):
 
       fft_data_raw = self.raw_buf[self.idx+1:self.idx+State.WINDOW+1]
       # remove DC offset
-      fft_data = fft_data_raw - fft_data_raw.mean()
-      f, fft = spsig.welch(fft_data, fs=128.0, nperseg=512, nfft=512)
-      if self.idx % 4 == 0:
-        plt.clear()
-        plt.semilogy(f, fft)
-        plt.ylim([0.5e-3, 1])
-        plt.xlabel('frequency [Hz]')
-        plt.ylabel('PSD [V**2/Hz]')
-        plt.show()
 
-    self.idx = (self.idx + 1) % State.WINDOW
+      fft_data = fft_data_raw - fft_data_raw.mean()
+      if self.idx % 64 == 0:
+        plot_fft(fft_data)
+
+      self.idx = (self.idx + 1) % State.WINDOW
 
   def set_state(self, state):
     if state == State.PROCESSING:
