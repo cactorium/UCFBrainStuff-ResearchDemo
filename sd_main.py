adb033b3a74ef1e3df7fa3ae6ebcbc1845757a3a
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-11-23 15:22:05 -0500

    Add senior design main script

--- sd_main.py
+++ sd_main.py
@@ -1,8 +1,11 @@
 import seniordesign_pb2
 
+from grpc.beta import implementations
+
 
 def main():
-  print (dir(seniordesign_pb2))
+  channel = implementations.insecure_channel('localhost', 500042)
+  stub = seniordesign_pb2.beta_create_ResultsReceiver_stub(channel)
 
 if __name__ == "__main__":
   main()
