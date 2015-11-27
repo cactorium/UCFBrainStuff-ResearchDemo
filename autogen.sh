dbad241efd1dc5affb840d17c5cc5620597e2bd6
Kelvin Ly <kelvin.ly1618@gmail.com>
2015-11-27 14:53:42 -0500

    Remove extra comment

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 
-# woo this will break if it's called from a different
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 protoc -I=$DIR --python_out=$DIR $DIR/seniordesign.proto
